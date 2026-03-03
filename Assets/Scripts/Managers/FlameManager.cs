3011f4b2b5226db2353d8db8725c6ab9bfa5e452
Lotu <nicolas@lotufo.com.ar>
2026-03-03 12:56:08 -0300

    feat: include Mallum Houses in entity cap count

--- Assets/Scripts/Managers/FlameManager.cs
+++ Assets/Scripts/Managers/FlameManager.cs
@@ -19,7 +19,7 @@ namespace Garden
             get
             {
                 var data = SaveManager.Instance.Data;
-                return data.plots.Count + data.vases.Count + data.gardens.Count;
+                return data.plots.Count + data.vases.Count + data.gardens.Count + data.mallumHouses.Count;
             }
         }
 
