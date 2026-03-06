390ed5aba57954057c5eac1a35998eab10734abb
Lotu <nicolas@lotufo.com.ar>
2026-03-06 11:09:40 -0300

    chore: disable rate limiting in dev environment

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -61,3 +61,6 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# Disable rate limiting in dev
+config :camp_fire, disable_rate_limit: true
